7fe051b64f0d3f7b2215ef827ef2e0c73967dd03
Johann Gislason <joigisla91@hotmail.com>
2014-10-13 22:44:50 +0000

    Added to calculate with new lines separting numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -7,7 +7,10 @@ public class Calculator {
 			return 0;
 		}
 		else if(text.contains(",")){
-			return sum(splitNumbers(text));
+			return sum(splitNumbersComma(text));
+		}
+		else if(text.contains("\n")){
+			return sum(splitNumbersNewLine(text));
 		}
 		else
 			return 1;
@@ -17,8 +20,12 @@ public class Calculator {
 		return Integer.parseInt(number);
 	}
 
-	private static String[] splitNumbers(String numbers){
+	private static String[] splitNumbersComma(String numbers){
 	    return numbers.split(",");
+	}
+
+	private static String[] splitNumbersNewLine(String numbers){
+	    return numbers.split("\n");
 	}
       
     private static int sum(String[] numbers){
