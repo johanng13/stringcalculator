9e3010a801b3417b275befa529d65cef85026ad1
Johann Gislason <joigisla91@hotmail.com>
2014-10-18 20:11:35 +0000

    Created test for multiple delimeters

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -69,4 +69,9 @@ public class CalculatorTest {
         assertEquals(6, Calculator.add("//[***]\n1***2***3"));
     }
 
+    @Test
+    public void testMultipleDelimiter(){
+        assertEquals(6, Calculator.add("//[*][%]\n1*2%3"));
+    }
+
 }
